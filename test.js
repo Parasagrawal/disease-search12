e3445b69bcb58673d66158cbabfd197914959fed
Parasagrawal <iit2019145@iiita.ac.in>
2021-04-19 10:09:25 +0530

    database added

---
+++ test.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const mysql = require('mysql')
+const app = express()
+
+var connection = mysql.createConnection({
+
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'company',
+    port: '3306'
+});
+
+connection.connect((error) => {
+    if (error) {
+        throw error
+    } else {
+        console.log('connected')
+    }
+})
+
+connection.query('INSERT INTO employees VALUES(5,"paras","india")', (error, rows) => {
+    if (error) {
+        throw error
+    } else {
+        console.log('inserted rows')
+        console.log(rows)
+    }
+})
+const port = process.env.PORT || 5000;
+app.listen(port)
+console.log(port)
+
